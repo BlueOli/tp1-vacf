1158c19b1880c6b90a36ee9b38614d09070e7247
Bruno Olaso Alaya <brunoolasoalaya@gmail.com>
2024-06-26 18:02:52 -0300

    Build 240626
    Menu now works as intended

--- Assets/Scripts/Menu/MenuManager.cs
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -9,7 +9,7 @@ public class MenuManager : MonoBehaviour
     {
         if (Input.anyKeyDown)
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            SceneManager.LoadScene(2);
         }
     }
 }
